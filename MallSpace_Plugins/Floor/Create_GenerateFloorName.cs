e4132a0c9928289e123fc26462d908b9fb689a1c
Giulia <giulia.chitic@endava.com>
2025-06-03 14:15:53 +0300

    Generating floor name on floor creation

--- MallSpace_Plugins/Floor/Create_GenerateFloorName.cs
+++ MallSpace_Plugins/Floor/Create_GenerateFloorName.cs
@@ -1,13 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace MallSpace_Plugins
 {
-    public class Create_GenerateFloorName
+    public class Create_GenerateFloorName : IPlugin
     {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)
+                serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity floor = (Entity)context.InputParameters["Target"];
+
+                try
+                {
+                    string mallName="", floorNumber;
+
+                    EntityReference mallRef = floor.GetAttributeValue<EntityReference>("giulia_mall");
+                    Entity mall = service.Retrieve("giulia_malls", mallRef.Id, new ColumnSet("giulia_name"));
+                    mallName = mall.GetAttributeValue<string>("giulia_name");
+
+                    floorNumber = floor.GetAttributeValue<int>("giulia_floornumber").ToString();
+                    floor["giulia_name"] = $"{mallName} Floor{floorNumber}";
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException("Error in Generate Floor Name plugin: " + ex.Message, ex);
+                }
+            }
+
+        }
     }
 }
