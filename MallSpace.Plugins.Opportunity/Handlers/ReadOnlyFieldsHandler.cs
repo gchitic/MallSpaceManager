280216d9a7de8781ee70a0396fd7f150f7899a44
Giulia <giulia.chitic@endava.com>
2025-06-05 14:10:56 +0300

    Added the plugin check for rentCost readOnly field

---
+++ MallSpace.Plugins.Opportunity/Handlers/ReadOnlyFieldsHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MallSpace.Plugins.Opportunity.Handlers
+{
+    public class ReadOnlyFieldsHandler
+    {
+        public void protectReadOnlyFields(IPluginExecutionContext context, Entity opportunity)
+        {
+            try 
+            {
+                if (opportunity.Contains("giulia_rentcost"))
+                {
+                    throw new InvalidPluginExecutionException("The Rent Cost field is read-only and cannot be set manually.");
+                }             
+            }
+            catch(InvalidPluginExecutionException ex)
+            {
+                throw new InvalidPluginExecutionException("Error: " + ex.Message, ex);
+            }
+            
+        }
+    }
+}
--- MallSpace.Plugins.Opportunity/Plugins/ReadOnlyFieldsPlugin.cs
+++ MallSpace.Plugins.Opportunity/Plugins/ReadOnlyFieldsPlugin.cs
@@ -8,34 +8,22 @@ using System.Threading.Tasks;
 
 namespace MallSpace.Plugins.Opportunity.Plugins
 {
-    public class OpportunityCreatePlugin : IPlugin
+    public class ReadOnlyFieldsPlugin : IPlugin
     {
         public void Execute(IServiceProvider serviceProvider)
         {
             IPluginExecutionContext context = (IPluginExecutionContext)
                 serviceProvider.GetService(typeof(IPluginExecutionContext));
             IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)
-                serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                serviceProvider.GetService(typeof (IOrganizationServiceFactory));
             IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
-            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            if(context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
                 Entity opportunity = (Entity)context.InputParameters["Target"];
 
-                try
-                {
-                    var handler = new Opportunity_CreateHandler();
-                    handler.Handle(opportunity);
-
-                }
-                catch (InvalidPluginExecutionException ex)
-                {
-                    throw new InvalidPluginExecutionException("Error in Opportunity Create Plugin: " + ex.Message, ex);
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message, ex);
-                }
+                var handler = new ReadOnlyFieldsHandler();
+                handler.protectReadOnlyFields(context, opportunity);
 
             }
         }
