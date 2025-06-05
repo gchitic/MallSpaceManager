5058b13d3352bed1f0cb5283076a028c224589f2
Giulia <giulia.chitic@endava.com>
2025-06-05 12:23:35 +0300

    Added the rent cost calculator for Opportunity entity

---
+++ MallSpace.Plugins.Opportunity/Business Logic/Opportunity_RentCostCaluclator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MallSpace.Plugins.Opportunity.Business_Logic
+{
+    public class Opportunity_RentCostCalculator
+    {
+        public decimal? Calculator(decimal? pricePerM2, decimal? offeredSpace)
+        {
+            if (!pricePerM2.HasValue && !offeredSpace.HasValue)
+                return null;
+
+            return pricePerM2 * offeredSpace;
+        }
+    }
+}
---
+++ MallSpace.Plugins.Opportunity/Plugins/OpportunityCreatePlugin.cs
@@ -0,0 +1,43 @@
+﻿using MallSpace.Plugins.Opportunity.Handlers;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MallSpace.Plugins.Opportunity
+{
+    public class OpportunityCreatePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)
+                serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity opportunity = (Entity)context.InputParameters["Target"];
+
+                try
+                {
+                    var handler = new Opportunity_CreateHandler();
+                    handler.Handle(opportunity);
+
+                }
+                catch (InvalidPluginExecutionException ex)
+                {
+                    throw new InvalidPluginExecutionException("Error in Opportunity Create Plugin: " + ex.Message, ex);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message, ex);
+                }
+
+            }
+        }
+    }
+}
